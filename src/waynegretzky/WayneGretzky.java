9813dc38eb20b658f3033a0f82d04df64d168765
aemal4075 <aemal4075@7JHN6Y1.Student.UGDSB.ED>
2018-09-27 10:12:47 -0400

    Wayne Gretzky for U2A1

---
+++ src/waynegretzky/WayneGretzky.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package waynegretzky;
+
+/**
+ *
+ * @author aemal4075
+ */
+public class WayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name; 
+        name = "Wayne Gretzky";
+        int num = 99;
+        System.out.println(name + "is number" + 99);
+        // TODO code application logic here
+    }
+    
+}
